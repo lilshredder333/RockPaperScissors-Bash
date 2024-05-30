c41bd8655973545d5b30b9551cf00bbc5171a70f
Nuri <147346467+lilshredder333@users.noreply.github.com>
2024-05-30 17:24:26 +0200

    made the code more functional and fixed all the bugs

--- rockpaperscissors.sh
+++ rockpaperscissors.sh
@@ -1,199 +1,160 @@
 #!/bin/bash
 
-
-
 puntos_usuario=0
 puntos_cpu=0
-marcador="CPU: $puntos_cpu vs $alias: $puntos_usuario"
-mensaje_final=""
+marcador=""
+alias=""
 contador_rondas=0
 
-#Generamos la opción random de la CPU
-function jugada_cpu(){
-    opciones=("piedra", "papel", "tijera")
-    jugada_cpu=${opciones[$((RANDOM % 3))]}
-    
+# Generamos la opción random de la CPU
+function jugada_cpu() {
+    opciones=("piedra" "papel" "tijera")
+    echo ${opciones[$((RANDOM % 3))]}
 }
 
- #Generamos la opción amañada de la CPU
- function jugadon_cpu(){
+# Generamos la opción amañada de la CPU
+function jugadon_cpu() {
+    local jugada_usuario=$1
+    case $jugada_usuario in
+        "piedra") echo "papel" ;;
+        "papel") echo "tijera" ;;
+        *) echo "piedra" ;;
+    esac
+}
 
-    jugada_usuario=$1
-
-    if [[ $jugada_usuario = "piedra" ]]; then
-            jugada_cpu="papel"
-            
-
-            elif [[ $jugada_usuario = "papel" ]]; then
-
-                jugada_cpu="tijera"
-                
-        else 
-
-            jugada_cpu="piedra"
-            
-        fi
- }
-
-#Funcion revisar ganador legal
-function quien_gana_legal(){
-    
-    jugada_usuario=$1
-    jugada_cpu=$2
+# Función para determinar el ganador de la ronda legal
+function quien_gana_legal() {
+    local jugada_usuario=$1
+    local jugada_cpu=$2
     
     if [[ $jugada_usuario == $jugada_cpu ]]; then
-        echo "Tremendo Empate"
-        
-        
-        elif [[ ($jugada_usuario == "piedra" && $jugada_cpu == "tijera") || ($jugada_usuario == "papel" && $jugada_cpu == "piedra") || ($jugada_usuario == "tijera" && $jugada_cpu == "papel") ]]; then
-        gana_user
-        
+        echo "Empate"
+    elif [[ ($jugada_usuario == "piedra" && $jugada_cpu == "tijera") || 
+            ($jugada_usuario == "papel" && $jugada_cpu == "piedra") || 
+            ($jugada_usuario == "tijera" && $jugada_cpu == "papel") ]]; then
+        echo "Ganaste"
     else
-        gana_cpu
+        echo "Perdiste"
     fi
-    
-    
 }
 
-#Funcion revisar ganador ilegal
-function quien_gana_ilegal(){
+# Función para actualizar el marcador y mostrar el resultado
+function actualizar_marcador() {
+    local resultado=$1
     
-    puntos_usuario=$1
-    puntos_cpu=$2
+    case $resultado in
+        "Ganaste")
+            puntos_usuario=$((puntos_usuario + 1))
+            ;;
+        "Perdiste")
+            puntos_cpu=$((puntos_cpu + 1))
+            ;;
+    esac
     
-
-}
-
-function gana_cpu(){
-    
-    echo "Ronda: $contador_rondas ¡Fin del juego, perdiste!"
-    puntos_cpu=$((puntos_cpu + 1))
     marcador="CPU: $puntos_cpu vs $alias: $puntos_usuario"
     echo $marcador
 }
 
-function gana_user(){
-    echo "Ronda: $contador_rondas ¡Fin del juego, ganaste!"
-    puntos_usuario=$((puntos_usuario + 1))
-    marcador="CPU: $puntos_cpu vs $alias: $puntos_usuario"
-    echo $marcador
-}
-
-function empate(){
-    echo "Ronda: $contador_rondas ¡Fin del juego, vaya empate más feo!"
-    marcador="CPU: $puntos_cpu vs $alias: $puntos_usuario"
-    echo $marcador
-}
-
+# Función para jugar en modo "Mejor de 5"
 function modo_5rondas() {
-    
-    echo "Hola "$alias", has elegido el modo de juego: Mejor de 5"
-    
+    echo "Hola $alias, has elegido el modo de juego: Mejor de 5"
     
     while [[ $contador_rondas -lt 5 ]]; do
-        echo "Ronda $contador_rondas - $marcador"
+        echo "Ronda $((contador_rondas + 1)) - $marcador"
         echo "Elige tu jugada (piedra, papel, tijera): "
         read jugada_usuario
         
-        jugada_cpu 
+        jugada_cpu=$(jugada_cpu)
+        echo "La CPU elige: $jugada_cpu"
         
-        echo "La CPU elige: " $jugada_cpu
+        resultado=$(quien_gana_legal $jugada_usuario $jugada_cpu)
+        echo $resultado
         
-        quien_gana_legal $jugada_usuario $jugada_cpu
+        actualizar_marcador $resultado
         
-        # Incrementa el contador de rondas
         contador_rondas=$((contador_rondas + 1))
     done
     
-    if [[ $contador_rondas -eq 5 ]]; then
-        
-        if [[ $puntos_cpu -gt $puntos_usuario ]]; then
-            
-            gana_cpu
-            
-            elif [[ $puntos_cpu -lt $puntos_usuario  ]]; then
-            
-            gana_user
-        else
-            empate
-        fi
+    if [[ $puntos_cpu -gt $puntos_usuario ]]; then
+        echo "¡Fin del juego, perdiste!"
+    elif [[ $puntos_usuario -gt $puntos_cpu ]]; then
+        echo "¡Fin del juego, ganaste!"
+    else
+        echo "¡Fin del juego, empate!"
     fi
 }
 
+# Función para jugar en modo "Infinito"
 function modo_infinito() {
-
-    echo "Hola "$alias", has elegido el modo de juego: Modo infinito"
+    echo "Hola $alias, has elegido el modo de juego: Infinito"
     
-    marcador="CPU: $puntos_cpu vs $alias: $puntos_usuario"
-    
-    while [[ 1 -eq 1 ]]; do
-        echo "Ronda $contador_rondas - $marcador"
+    while true; do
+        echo "Ronda $((contador_rondas + 1)) - $marcador"
         echo "Elige tu jugada (piedra, papel, tijera): "
         read jugada_usuario
         
-        jugada_cpu
+        jugada_cpu=$(jugada_cpu)
+        echo "La CPU elige: $jugada_cpu"
         
-        echo "La CPU elige: " $jugada_cpu
+        resultado=$(quien_gana_legal $jugada_usuario $jugada_cpu)
+        echo $resultado
         
-        quien_gana_legal $jugada_usuario $jugada_cpu $puntos_usuario $puntos_cpu
+        actualizar_marcador $resultado
         
-        # Incrementa el contador de rondas
         contador_rondas=$((contador_rondas + 1))
     done
 }
 
+# Función para jugar en modo "Imposible"
 function modo_imposible() {
+    echo "Hola $alias, has elegido el modo de juego: Imposible. Espero que sepas manejar tu frustración"
     
-    
-    echo "Hola "$alias", has elegido el modo de juego: Imposible. Espero que sepas manejar tu frustración"
-    
-    marcador="CPU: $puntos_cpu vs $alias: $puntos_usuario"
-    
-    while [[ 1 -eq 1 ]]; do
-        echo "Ronda $contador_rondas - $marcador"
+    while true; do
+        echo "Ronda $((contador_rondas + 1)) - $marcador"
         echo "Elige tu jugada (piedra, papel, tijera): "
         read jugada_usuario
         
-        jugadon_cpu $jugada_usuario
+        jugada_cpu=$(jugadon_cpu $jugada_usuario)
+        echo "La CPU elige: $jugada_cpu"
         
-        echo "La CPU elige: " $jugada_cpu
+        puntos_cpu=$((puntos_cpu + 1))
+        marcador="CPU: $puntos_cpu vs $alias: $puntos_usuario"
+        echo $marcador
         
-        quien_gana_ilegal $puntos_usuario $puntos_cpu
-        
-        # Incrementa el contador de rondas
         contador_rondas=$((contador_rondas + 1))
     done
-    
-}  
+}
 
-function elegir_dificultad(){
+# Función para elegir la dificultad
+function elegir_dificultad() {
     echo "Introduce tu alias:"
     read alias
-    echo "Elije el modo de juego (Mejor de 5, Infinito o Imposible): "
+    
+    echo "Elije el modo de juego (Mejor de 5, Infinito, Imposible): "
     read eleccion_dificultad
     
-    # nocasematch para q sea mas user friendly :)
+    # nocasematch para que sea más user-friendly
     shopt -s nocasematch
     
     case $eleccion_dificultad in
         "mejor de 5")
             modo_5rondas
-        ;;
+            ;;
         "infinito")
             modo_infinito
-        ;;
+            ;;
         "imposible")
             modo_imposible
-        ;;
+            ;;
         *)
             echo "Opción no válida"
-        ;;
+            ;;
     esac
     
-    # desactivamos
+    # Desactivamos nocasematch
     shopt -u nocasematch
 }
 
-#Inicio del juego
+# Inicio del juego
 echo "Bienvenido al juego de Piedra, Papel, Tijera"
 elegir_dificultad
